3a3da6d72378f2cab3a471103b5afa46fa919299
rubenyepez-rym <ryepezmoreano@yahoo.com>
2024-05-25 00:05:42 -0500

    hecho

--- app/controller/AlumnoCotroller.js
+++ app/controller/AlumnoCotroller.js
@@ -18,6 +18,6 @@ class AlumnoController{
         res.json (await Alumno.listar());
 
     }
-//29:51 esta es mi prueba hecha en remoto
+//29:51 Ahora solo hecho en local
 }
 module.exports=new AlumnoController;
